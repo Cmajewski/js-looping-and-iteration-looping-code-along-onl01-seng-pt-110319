3b30789a7cefbee6886252b4b811ceb0fe9f73ca
Caitlin Majewski <cmajewski@wesleyan.edu>
2020-07-11 20:34:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const names=["Caitlin", "Taylor", "Brooke"]
-const event= "Wedding"
-const array=[]
+
 function writeCards(names,event) {
+  let array=[]
 for (let i=0;i<names.length;i++){
   array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
 }
