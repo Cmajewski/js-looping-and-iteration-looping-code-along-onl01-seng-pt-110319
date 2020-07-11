8045fdbddd4e3d897cea1d7b94097934e12974cc
Caitlin Majewski <cmajewski@wesleyan.edu>
2020-07-11 20:31:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names=["Caitlin", "Taylor", "Brooke"]
+const event= "Wedding"
+const array=[]
+function writeCards(names,event) {
+for (let i=0;i<names.length;i++){
+  array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+}
+return array
+}
+
+
+function countDown(integer){
+  while(integer>-1){
+    console.log(integer--);
+  }
+}
