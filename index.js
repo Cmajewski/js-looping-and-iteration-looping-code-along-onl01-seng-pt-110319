4df165bb4ba4e7400953d835198aef859a626cbb
Caitlin Majewski <cmajewski@wesleyan.edu>
2020-12-15 00:29:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function writeCards(name, greeting){
 let thankYouCards=[]
 for (let i=0; i<name.length; i++){
-  thankYouCards.push(`Thank you ${name[i]}! For the wonderful ${greeting} gift`)
+  thankYouCards.push(`Thank you ${name[i]}! For the wonderful ${greeting} gift!`)
 }
 return thankYouCards
 }
