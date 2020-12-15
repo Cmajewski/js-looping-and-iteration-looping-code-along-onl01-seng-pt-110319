a7c97b961f49ad77c79ad37486802e9d9bc5dd3e
Caitlin Majewski <cmajewski@wesleyan.edu>
2020-12-15 00:36:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ return thankYouCards
 
 function countDown(startingNumber){
 while (startingNumber>0)
-  {console.log `startingNumber`
-  startingNumber-1}
+  {console.log (startingNumber)
+    startingNumber--
+  }
 }
