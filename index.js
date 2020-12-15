77db07cfafc663812fdc956cb6b956c002e5f2fe
Caitlin Majewski <cmajewski@wesleyan.edu>
2020-12-15 00:13:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-
-function writeCards(names,event) {
-  let array=[]
-for (let i=0;i<names.length;i++){
-  array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-}
-return array
-}
-
-
-function countDown(integer){
-  while(integer>-1){
-    console.log(integer--);
-  }
+const writeCards(names){
+  names.each(function(e){
+    console.log(`Thank you ${e}`)
+  })
 }
